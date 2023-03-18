2af250182818707436c0773793ce96b7520c7b1f
andrey-satyukov <1846120@gmail.com>
2023-03-18 20:16:17 +0500

    исправленное д/з

--- src/main/java/pro/sky/calculator/CalculatorSericeImpl.java
+++ src/main/java/pro/sky/calculator/CalculatorSericeImpl.java
@@ -10,18 +10,18 @@ public class CalculatorSericeImpl implements CalculatorService {
     }
 
     public String addition(Integer num1, Integer num2) {
-            return Integer.toString(num1 + num2);
+        return Integer.toString(num1 + num2);
     }
 
     public String minus(Integer num1, Integer num2) {
-            return Integer.toString(num1 - num2);
+        return Integer.toString(num1 - num2);
     }
 
     public String multiply(Integer num1, Integer num2) {
-            return Integer.toString(num1 * num2);
+        return Integer.toString(num1 * num2);
     }
 
     public String divide(Double num1, Double num2) {
-            return Double.toString(num1 / num2);
+        return Double.toString(num1 / num2);
     }
 }
